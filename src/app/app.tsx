f5126588b733a2066ffc28926abc727d708d004b
irenlian <irynalian@stockx.com>
2020-08-08 16:02:23 +0300

    Add header and sidebar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 
+import Sidebar from '~/components/Sidebar/Sidebar';
 import Catalog from '../components/Catalog/Catalog';
 import NotFound from '~/components/NotFound/NotFound';
 import CreateRenter from '~/components/NewRenter/CreateRenter';
 import Renter from '~/components/Renter/Renter';
 
 const App: React.FunctionComponent = () => (
-  <Switch>
-    <Route exact path="/" component={Catalog} />
-    <Route exact path="/create" component={CreateRenter} />
-    <Route exact path="/ad/:id" component={Renter} />
-    <Route path="/" component={NotFound} />
-  </Switch>
+  <div className="root">
+    <Sidebar />
+    <main className="content">
+      <div className="toolbar" />
+      <Switch>
+        <Route exact path="/" component={Catalog} />
+        <Route exact path="/create" component={CreateRenter} />
+        <Route exact path="/ad/:id" component={Renter} />
+        <Route path="/" component={NotFound} />
+      </Switch>
+    </main>
+  </div>
 );
 
 export default App;
